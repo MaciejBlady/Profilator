25f6d1fd4b2d0d01fe90ededdd69fd253ed6a53c
MaciejBlady <maciej.blady@gmail.com>
2017-10-28 00:09:25 +0200

    MemoryModule prototype

---
+++ Assets/Scripts/Profilator/Modules/MemoryModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace Profilator
+{
+    public class MemoryModule : ProfilatorModule
+    {
+        public override ProfilatorDataRecord GetData()
+        {
+            ProfilatorDataRecord data = new ProfilatorDataRecord();
+            data.AddData("AllocatedKBytes", (GC.GetTotalMemory(true) / (1024)).ToString());
+            return data;         
+        }
+    }
+}
