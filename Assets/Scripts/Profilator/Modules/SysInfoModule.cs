38d4c56995a4cd785ec5c871119d14e38ca0276e
MaciejBlady <maciej.blady@gmail.com>
2017-10-28 00:09:11 +0200

    SysInfoModule prototype

---
+++ Assets/Scripts/Profilator/Modules/SysInfoModule.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Profilator
+{
+    public class SysInfoModule : ProfilatorModule
+    {
+        public override ProfilatorDataRecord GetData()
+        {
+            ProfilatorDataRecord data = new ProfilatorDataRecord();
+            data.AddData("System", SystemInfo.operatingSystem);
+            data.AddData("ProcessorCount",SystemInfo.processorCount.ToString());
+            data.AddData("GraphicsMemory", SystemInfo.graphicsMemorySize.ToString());
+            data.AddData("SystemMemory", SystemInfo.systemMemorySize.ToString());
+            return data;
+        }
+    }
+}
