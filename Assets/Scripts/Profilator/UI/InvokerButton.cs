bfc91f4221b9f6ccef3d6e073ca79cf1d460141d
MaciejBlady <maciej.blady@gmail.com>
2017-10-30 23:51:03 +0100

    Invoker window scripts

---
+++ Assets/Scripts/Profilator/UI/InvokerButton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+namespace Profilator
+{
+    [RequireComponent(typeof(Button))]
+    public class InvokerButton : MonoBehaviour
+    {
+        private Text _label;
+        private Button _properButton;
+
+        public Button ProperButton
+        {
+            get
+            {
+                if (_properButton == null)
+                {
+                    _properButton = GetComponent<Button>();
+                }
+                return _properButton;
+            }
+        }
+
+        public Button.ButtonClickedEvent OnClick
+        {
+            get
+            {
+                return ProperButton.onClick;
+            }
+
+            set
+            {
+                ProperButton.onClick = value;
+            }
+        }
+
+        public string Label
+        {
+            get
+            {
+                return _label != null ? _label.text : string.Empty;
+            }
+
+            set
+            {
+                if (_label != null)
+                {
+                    _label.text = value;
+                }            
+            }
+        }
+
+        private void Awake()
+        {
+            _label = GetComponentInChildren<Text>();
+        }
+    } 
+}
---
+++ Assets/Scripts/Profilator/UI/InvokerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Profilator
+{
+    public class InvokerController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _invokerButtonPrefab;
+        [SerializeField]
+        private GameObject _buttonParent;
+        [SerializeField]
+        private Button.ButtonClickedEvent[] _buttonEvents;
+
+        private void Awake()
+        {
+            foreach (var buttonEvent in _buttonEvents)
+            {
+                AddButton(buttonEvent);
+            }
+        }
+
+        public void AddButton(Button.ButtonClickedEvent buttonEvent)
+        {
+            GameObject newButton = Instantiate<GameObject>(_invokerButtonPrefab);
+            if (newButton != null)
+            {
+                newButton.transform.parent = _buttonParent.transform;
+                InvokerButton invokerButton = newButton.GetComponent<InvokerButton>();
+                if (invokerButton != null)
+                {
+                    invokerButton.OnClick = buttonEvent;
+                    invokerButton.Label = buttonEvent.GetPersistentTarget(0).ToString();
+                }   
+            }
+        }
+    } 
+}
