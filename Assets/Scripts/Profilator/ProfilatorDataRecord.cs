09129f52dc12b8a6e82728303b37959227b7a7bd
MaciejBlady <maciej.blady@gmail.com>
2017-10-31 21:05:53 +0100

    ProfilatorDataRecord rework

--- Assets/Scripts/Profilator/ProfilatorDataRecord.cs
+++ Assets/Scripts/Profilator/ProfilatorDataRecord.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Profilator
 {
-    public class ProfilatorDataRecord : IEnumerable<KeyValuePair<string, string>>
+    public class ProfilatorDataRecord : IEnumerable<KeyValuePair<string, string>>, IProfilatorData
     {
         private DateTime _created;
         private Dictionary<string, string> _records = new Dictionary<string, string>();
 
-        public ProfilatorDataRecord()
+        public DateTime Created
         {
-            _created = DateTime.Now;
+            get
+            {
+                return _created;
+            }
         }
 
-        public void WriteToFile(string path)
+        public ProfilatorModule Source
         {
+            get;
+            private set;
+        }
 
+
+        public ProfilatorDataRecord(ProfilatorModule source)
+        {
+            Source = source;
+            _created = DateTime.Now;
         }
 
         public void AddData(string label, string value)
@@ -26,12 +38,14 @@ namespace Profilator
             _records.Add(label, value);
         }
 
-        public override string ToString()
+        public string GetFormattedData()
         {
             StringBuilder recordString = new StringBuilder();
+            recordString.AppendLine(string.Format("Module: {0}", Source.name));
+            recordString.AppendLine(string.Format("Created: {0}", Created));
             foreach (var record in this)
             {
-                recordString.Append(string.Format("{0}: {1}\n", record.Key, record.Value.ToString()));
+                recordString.AppendLine(string.Format("{0}: {1}", record.Key, record.Value.ToString()));
             }
             return recordString.ToString();
         }
