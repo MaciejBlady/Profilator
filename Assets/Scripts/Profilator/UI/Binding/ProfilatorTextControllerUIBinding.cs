fa7f8e8a9b81e6ee5049f64b0d18ae02ff72fd52
MaciejBlady <maciej.blady@gmail.com>
2017-11-03 00:45:16 +0100

    Minor UI scripts added

---
+++ Assets/Scripts/Profilator/UI/Binding/ProfilatorTextControllerUIBinding.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Profilator
+{
+    [RequireComponent(typeof(ProfilatorTextController))]
+    public class ProfilatorTextControllerUIBinding : MonoBehaviour
+    {
+        private ProfilatorTextController _controller;
+        public ProfilatorTextController Controller
+        {
+            get
+            {
+                if (_controller == null)
+                {
+                    _controller = GetComponent<ProfilatorTextController>();
+                }
+                return _controller;
+            }
+        }
+
+        [SerializeField]
+        private InputField _inputField;
+        [SerializeField]
+        private Toggle _toggle;
+
+        private void Awake()
+        {
+            if (_inputField != null)
+            {
+                _inputField.text = Controller.DataSampleFrameInterval.ToString();
+                _inputField.onEndEdit.AddListener(SetConvertedInput);
+            }
+
+            if (_toggle != null)
+            {
+                _toggle.isOn = Controller.LogSampledData;
+                _toggle.onValueChanged.AddListener(SetToggle);
+            }
+        }
+
+        private void SetConvertedInput(string input)
+        {
+            int converted = 0;
+            if(int.TryParse(input, out converted) && converted > -1)
+            {
+                Controller.DataSampleFrameInterval = converted;
+            }
+        }
+
+        private void SetToggle(bool isOn)
+        {
+            Controller.LogSampledData = isOn;
+        }
+    } 
+}
---
+++ Assets/Scripts/Profilator/UI/Converters/StringToIntConverter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Profilator
+{
+    public class StringToIntConverter : MonoBehaviour
+    {
+        [System.Serializable]
+        private class SetIntEvent : UnityEvent<int> { };
+        [SerializeField]
+        private SetIntEvent _setInt;
+        [SerializeField]
+        private bool _allowNegative = false;
+
+        public string ToConvert
+        {
+            set
+            {
+                int converted = 0;
+                bool canUse = int.TryParse(value, out converted) && (converted > 0 || _allowNegative);
+                _setInt.Invoke(canUse ? converted : 1);
+            }
+        }
+    } 
+}
