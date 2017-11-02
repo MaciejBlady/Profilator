9af34846374ba3593bb3c807d0b19e765e24dbed
MaciejBlady <maciej.blady@gmail.com>
2017-11-03 00:42:55 +0100

    Console logger added

---
+++ Assets/Scripts/Profilator/Logging/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Profilator
+{
+    [CreateAssetMenu(menuName = "ProfilatorLoggers/ConsoleLogger")]
+    public class ConsoleLogger : ProfilatorLogger
+    {
+        public override void Log(IProfilatorData data)
+        {
+            Debug.Log(data.GetFormattedData());
+        }
+    } 
+}
