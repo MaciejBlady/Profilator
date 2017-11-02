9bb6f45b623d2e3a7cd56bd8ad05dcb155d84de9
MaciejBlady <maciej.blady@gmail.com>
2017-11-03 00:44:38 +0100

    Unity profiler module added
    CPUModule reworked to be able to show all unity profiler recorders data

---
+++ Assets/Scripts/Profilator/Modules/UnityProfilerModule.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Profiling;
+using System.Collections.Generic;
+
+namespace Profilator
+{
+    [CreateAssetMenu(menuName = "ProfilatorModules/UnityProfilerModule")]
+    public class UnityProfilerModule : ProfilatorModule
+    {
+        [SerializeField]
+        private string[] _profilerRecorderNames;
+        private Dictionary<string, Recorder> _recorders = new Dictionary<string, Recorder>();
+
+        private void OnEnable()
+        {
+            foreach (var _recorderName in _profilerRecorderNames)
+            {
+                AddRecorder(_recorderName);
+            }
+        }
+
+        private void AddRecorder(string name)
+        {
+            Recorder recorder = Recorder.Get(name);
+            recorder.enabled = true;
+            _recorders.Add(name, recorder);
+        }
+
+        public override IProfilatorData GetData()
+        {
+            ProfilatorDataRecord data = new ProfilatorDataRecord(this);
+
+            foreach (var recorder in _recorders)
+            {
+                data.AddData(recorder.Key, recorder.Value.elapsedNanoseconds.ToString());
+            }
+            
+            return data;
+        }
+    }
+}
